c9c7ed3ab81a40c1eba629250fc663cb43b61856
Bryan Perez <bperez21@binghamton.edu>
2025-11-07 15:16:56 -0500

    Added layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Layout } from "./components/layouts/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
@@ -6,11 +7,13 @@ import Profile from "./pages/Profile";
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 }
